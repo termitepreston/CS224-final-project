64e069a2f34247c3b2eab66ce4b8aa5f6bb3d8ab
Alazar G <alazar4018@gmail.com>
2023-07-26 15:12:13 +0300

    initial commit.

---
+++ src/main/java/io/github/termitepreston/schoolprojects/App.java
@@ -0,0 +1,13 @@
+package io.github.termitepreston.schoolprojects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
